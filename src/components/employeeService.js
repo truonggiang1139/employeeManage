860bd6dfca80731ec3624290d064bf2916aaf3a5
Hoang Giang <truonggiang1139@gmail.com>
2023-03-22 14:21:55 +0700

    update api call

--- src/components/employeeService.js
+++ src/components/employeeService.js
@@ -1,38 +1,38 @@
 import axios from "axios";
 export const getEmployeeData = () => {
   return axios.post(
-    "http://training-api.oceantech.com.vn/cms/employees/search",
+    "https://training-api.oceantech.com.vn/cms/employees/search",
     {}
   );
 };
 export const updateEmployee = (employee) => {
   return axios.put(
-    `http://training-api.oceantech.com.vn/cms/employees/${employee.id}`,
+    `https://training-api.oceantech.com.vn/cms/employees/${employee.id}`,
     employee
   );
 };
 export const addEmployee = (employee) => {
   return axios.post(
-    "http://training-api.oceantech.com.vn/cms/employees",
+    "https://training-api.oceantech.com.vn/cms/employees",
     employee
   );
 };
 
 export const deleteEmployee = (id) => {
   return axios.delete(
-    "http://training-api.oceantech.com.vn/cms/employees/" + id
+    "https://training-api.oceantech.com.vn/cms/employees/" + id
   );
 };
 
 export const searchEmployee = (value) => {
   if (/[0-9]/.test(value)) {
     return axios.post(
-      "http://training-api.oceantech.com.vn/cms/employees/search",
+      "https://training-api.oceantech.com.vn/cms/employees/search",
       { code: value }
     );
   }
   return axios.post(
-    "http://training-api.oceantech.com.vn/cms/employees/search",
+    "https://training-api.oceantech.com.vn/cms/employees/search",
     { name: value }
   );
 };
